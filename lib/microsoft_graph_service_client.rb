26c77d9624f5bd047f1892a27d0d96c9ea2bd7fe
Vincent Biret <vibiret@microsoft.com>
2023-02-02 13:12:48 -0500

    - renames request adapter and client to align with other languages

--- lib/microsoft_graph_service_client.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'microsoft_graph_base_service_client'
-module MicrosoftGraph
-	class MicrosoftGraphServiceClient < MicrosoftGraph::MicrosoftGraphBaseServiceClient
-		#TODO define sugar constructors acception the authentication provider
-		#TODO shim me
-	end
-end
